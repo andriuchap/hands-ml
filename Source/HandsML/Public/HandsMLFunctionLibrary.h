b51f413f6994c67860783eb26adb299776c1477b
lukas <lukas>
2023-05-22 17:59:35 +0300

    implement function activation with buttons, button feedback

---
+++ Source/HandsML/Public/HandsMLFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "HandsMLFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HANDSML_API UHandsMLFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
